16945fab1a74da56aabce31ad5cb8d543c0a7479
tomas briones <tomasbriones@gmail.com>
2021-11-23 16:05:56 -0300

    bd setting change

--- sinergia/sinergia/settings.py
+++ sinergia/sinergia/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'sinergia',
         'USER': 'tbriones',
-        'PASSWORD': 'secreto',
+        'PASSWORD': 'tomas',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
