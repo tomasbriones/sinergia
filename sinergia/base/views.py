1301f3b0efc3ce7c9c669e385c8dcd2bbdb28f97
tomas briones <tomasbriones@gmail.com>
2021-11-23 17:14:00 -0300

    define about and home in base view

--- sinergia/base/views.py
+++ sinergia/base/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+html_base = """ 
+    <h1>Sinergia</h1>
+    <ul>
+        <li><a href="/">Portada</a></li>
+        <li><a href="/about/">Acerca de</a></li>
+    </ul>
+"""
+
+def home(request):
+    return HttpResponse(html_base + "<h2>Portada</h2>")
+
+def about(request):
+    return HttpResponse(html_base + "<h2>Acerca de</h2>")
