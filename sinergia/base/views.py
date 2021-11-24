6104b7ebf410fa53cc51a46bdfa59a4fddbebd21
tomas briones <tomasbriones@gmail.com>
2021-11-24 00:54:46 -0300

    add templates and methods in view

--- sinergia/base/views.py
+++ sinergia/base/views.py
@@ -2,16 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-html_base = """ 
-    <h1>Sinergia</h1>
-    <ul>
-        <li><a href="/">Portada</a></li>
-        <li><a href="/about/">Acerca de</a></li>
-    </ul>
-"""
-
 def home(request):
-    return HttpResponse(html_base + "<h2>Portada</h2>")
+    return render(request,"base/home.html")
 
 def about(request):
-    return HttpResponse(html_base + "<h2>Acerca de</h2>")
+    return render(request,"base/about.html")
